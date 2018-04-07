30ebee30feadcce52991dc855b38859335272857
artifishional <artifishional@mail.ru>
2018-04-08 01:01:20 +0300

    - resources resolver - added

--- src/resolver.js
+++ src/resolver.js
@@ -14,8 +14,10 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
         app.get(`/${dir}*`, function(req, res) {
 
             let m2mode = "js";
+            let name;
+            let m2file;
 
-            let [name] = "".match.call(
+            [name] = "".match.call(
                 req.params[0], /^([a-z0-9]{1,20}[\-_]{0,1}[a-z0-9]{1,20}){1,5}\.js$/g
             ) || [];
 
@@ -27,7 +29,23 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                     req.params[0], /^([a-z0-9]{1,20}[\-_]{0,1}[a-z0-9]{1,20}){1,5}\/index\.json$/g
                 ) || [];
 
+                if(!name) {
+
+                    m2mode = "res";
+
+                    [name] = "".match.call(
+                        req.params[0], /^[a-z0-9_\-]{1,25}\/res\/[a-z0-9\-_]{1,25}\.[a-z0-9]{3,4}$/g
+                    ) || [];
+
+                    if(!name) throw `unexpected module request "${req.params[0]}"`;
+
+                    [name] = "".match.call( req.params[0], /^[a-z\-_0-9]{1,25}/g ) || [];
+                    [m2file] = "".match.call( req.params[0], /\/[a-z\-_0-9]{1,25}\.[a-z0-9]{3,4}$/g ) || [];
+
+                    if(!name) throw `unexpected module name "${req.params[0]}"`;
+                }
                 if(!name) throw `unexpected module name "${req.params[0]}"`;
+
 
                 name = name.replace( "/index.json", "" );
 
@@ -37,11 +55,14 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                 name = name.replace( ".js", "" );
             }
 
-
             const output = path.resolve(dirname, `./../../../node_modules/${name}/m2unit/`);
             const input =
                 path.resolve(dirname,
-                    `./../../../node_modules/${name}/src/${m2mode === "js" ? "index.js" : "index.json"}`
+                    `./../../../node_modules/${name}/src/${
+                        m2mode === "js"   ? "index.js" :
+                            m2mode === "json" ? "index.json" :
+                                "../res" + m2file
+                    }`
                 );
 
             const module = path.resolve(dirname, `./../../../node_modules/${name}`);
@@ -97,8 +118,14 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                     });
                 }
             }
-            else {
+            else if(m2mode === "json") {
                 fs.readFile(`${input}`, "utf8", (err, data) => {
+                    if (err) throw err;
+                    res.send(data);
+                });
+            }
+            else if(m2mode === "res") {
+                fs.readFile(`${input}`, (err, data) => {
                     if (err) throw err;
                     res.send(data);
                 });
