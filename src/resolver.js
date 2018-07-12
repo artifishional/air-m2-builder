e4d84649eba52178d5c3803a4dcddd12e62f6375
Полицинский Артем Сергеевич <as_policinskij@bingo-boom.ru>
2018-07-12 17:19:05 +0300

    - html sup - added

--- src/resolver.js
+++ src/resolver.js
@@ -5,7 +5,7 @@ import {execSync} from "child_process"
 import {Observable} from "air-stream"
 import m2builderConf from "../webpack.m2builder.config"
 
-export default function after({ dirname, mode, m2units: { units, dir = "m2units/" } }) {
+export default function after({dirname, mode, m2units: {units, dir = "m2units/"}}) {
 
     return function (app) {
 
@@ -17,15 +17,11 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
             let name;
             let m2file;
 
-
-
-
-
             [name] = "".match.call(
                 req.params[0], /^([a-z0-9]{1,20}[\-_]{0,1}[a-z0-9]{1,20}){1,5}\.js$/g
             ) || [];
 
-            if(!name) {
+            if (!name) {
 
                 m2mode = "json";
 
@@ -33,49 +29,68 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                     req.params[0], /^([a-z0-9]{1,20}[\-_]{0,1}[a-z0-9]{1,20}){1,5}\/index\.json$/g
                 ) || [];
 
-                if(!name) {
 
-                    m2mode = "res";
+                if (!name) {
+
+                    m2mode = "html";
 
                     [name] = "".match.call(
-                        req.params[0], /^[a-z0-9_\-]{1,25}\/res\/[a-z0-9\-_]{1,25}\.[a-z0-9]{3,4}$/g
+                        req.params[0], /^([a-z0-9]{1,20}[\-_]{0,1}[a-z0-9]{1,20}){1,5}\/index\.html/g
                     ) || [];
 
-                    if(!name) throw `unexpected module request "${req.params[0]}"`;
+                    if (!name) {
 
-                    [name] = "".match.call( req.params[0], /^[a-z\-_0-9]{1,25}/g ) || [];
-                    [m2file] = "".match.call( req.params[0], /\/[a-z\-_0-9]{1,25}\.[a-z0-9]{3,4}$/g ) || [];
+                        m2mode = "res";
 
-                    if(!name) throw `unexpected module name "${req.params[0]}"`;
+                        [name] = "".match.call(
+                            req.params[0], /^[a-z0-9_\-]{1,25}\/res\/[a-z0-9\-_]{1,25}\.[a-z0-9]{3,4}$/g
+                        ) || [];
+
+                        if (!name) throw `unexpected module request "${req.params[0]}"`;
+
+                        [name] = "".match.call(req.params[0], /^[a-z\-_0-9]{1,25}/g) || [];
+                        [m2file] = "".match.call(req.params[0], /\/[a-z\-_0-9]{1,25}\.[a-z0-9]{3,4}$/g) || [];
+
+                        if (!name) throw `unexpected module name "${req.params[0]}"`;
+                    }
+
+                    else {
+
+                        name = name.replace("/index.html", "");
+
+                    }
+
+
                 }
-                if(!name) throw `unexpected module name "${req.params[0]}"`;
 
-
-                name = name.replace( "/index.json", "" );
+                else {
+                    name = name.replace("/index.json", "");
+                }
 
             }
 
             else {
-                name = name.replace( ".js", "" );
+                name = name.replace(".js", "");
             }
 
             const output = path.resolve(dirname, `./../../../node_modules/${name}/m2unit/`);
             const input =
                 path.resolve(dirname,
                     `./../../../node_modules/${name}/src/${
-                        m2mode === "js"   ? "index.js" :
+                        m2mode === "js" ? "index.js" :
                             m2mode === "json" ? "index.json" :
-                                "../res" + m2file
-                    }`
+                                m2mode === "html" ? "index.html" :
+                                    "../res" + m2file
+                        }`
                 );
 
             const module = path.resolve(dirname, `./../../../node_modules/${name}`);
 
-            if(!fs.existsSync(module)) {
-                const unit = units.find( ({name: _name}) => name === _name );
-                if(!unit) throw `Requested unit "${name}" is not among m2units`;
+            if (!fs.existsSync(module)) {
+                const unit = units.find(({name: _name}) => name === _name);
+                if (!unit) throw `Requested unit "${name}" is not among m2units`;
                 console.log(`preinstall "${name}"...`);
-                execSync(`npm install ${unit.npm} --no-save` );
+                execSync(`npm install ${unit.npm} --no-save`);
                 console.log(`preinstall "${name}" - ok`);
             }
 
@@ -102,8 +117,8 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
 
             */
 
-            if(m2mode === "js") {
-                if(fs.existsSync(`${output}/index.js`)) {
+            if (m2mode === "js") {
+                if (fs.existsSync(`${output}/index.js`)) {
                     fs.readFile(`${output}/index.js`, "utf8", (err, data) => {
                         if (err) throw err;
                         res.send(data);
@@ -113,7 +128,7 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                     console.log(`compile "${name}"...`);
                     const compiler = webpack(m2builderConf({input, mode, output}));
                     compiler.run((err) => {
-                        if(err) throw err;
+                        if (err) throw err;
                         fs.readFile(`${output}/index.js`, "utf8", (err, data) => {
                             if (err) throw err;
                             console.log(`compile "${name}" - ok`);
@@ -122,13 +137,19 @@ export default function after({ dirname, mode, m2units: { units, dir = "m2units/
                     });
                 }
             }
-            else if(m2mode === "json") {
+            else if (m2mode === "json") {
                 fs.readFile(`${input}`, "utf8", (err, data) => {
                     if (err) throw err;
                     res.send(data);
                 });
             }
-            else if(m2mode === "res") {
+            else if (m2mode === "html") {
+                fs.readFile(`${input}`, "utf8", (err, data) => {
+                    if (err) throw err;
+                    res.send(data);
+                });
+            }
+            else if (m2mode === "res") {
                 fs.readFile(`${input}`, (err, data) => {
                     if (err) throw err;
                     res.send(data);
