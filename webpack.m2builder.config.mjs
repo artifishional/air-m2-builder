59767b99f91d31f549df330c547bf85951d4917e
Полицинский Артем Сергеевич <artifishional@mail.ru>
2018-10-17 15:08:49 +0300

    - debug mode with proxy - added

--- webpack.m2builder.config.mjs
+++ webpack.m2builder.config.mjs
@@ -1,14 +1,21 @@
-export default ({input, mode, output, name = `[name]`}) => {
+export default ({input, mode, output, name = `[name]`, debug = false }) => {
+
+    const entry = {
+        [ name ]: [ input ],
+    };
+
+    if(debug) {
+        entry.debug = [ "./debug/" + input ]
+    }
+
     return ({
         devtool: "(none)",
         mode,
-        entry: {
-            'index': [input]
-        },
+        entry,
         externals: { m2: '__M2' },
         output: {
             path: output,
-            filename: `${name}/index.js`,
+            filename: `[name]/index.js`,
             library: "m2unit",
             libraryTarget: "this"
         },/*
